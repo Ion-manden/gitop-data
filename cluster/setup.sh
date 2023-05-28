468230415e6624091061dcb106e6ee2d45548074
Nicolai Thomassen <nicolai-thomassen@hotmail.com>
2023-05-28 13:41:25 +0200

    improve setup script

--- cluster/setup.sh
+++ cluster/setup.sh
@@ -1,2 +1,11 @@
 k3d cluster create gitops-data-cluster --servers 3 --agents 3 --wait
 
+# Create argocd namespace
+kubectl apply -f ../infra/argo/cd/namespace.yaml
+
+# Install argo cd
+kubectl apply -f ../infra/argo/cd/install.yaml
+
+# Add top level argo cd application resource - this will auto discover all other applications
+kubectl apply -f ../infra/application.yaml
+
