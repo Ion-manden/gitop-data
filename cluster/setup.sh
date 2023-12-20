3aac95ad8db027f6dde62f4b4169b984d591ec72
Nicolai Thomassen <nicolai-thomassen@hotmail.com>
2023-12-20 15:16:56 +0100

    updated setup

--- cluster/setup.sh
+++ cluster/setup.sh
@@ -1,11 +1,14 @@
-k3d cluster create gitops-data-cluster --servers 3 --agents 3 --wait
+# k3d cluster create gitops-data-cluster --servers 3 --agents 3 --wait
 
 # Create argocd namespace
-kubectl apply -f ../infra/argo/cd/namespace.yaml
+KUBECONFIG=/etc/rancher/k3s/k3s.yaml kubectl apply -f ../infra/argo/cd/namespace.yaml
 
 # Install argo cd
-kubectl apply -f ../infra/argo/cd/install.yaml
+KUBECONFIG=/etc/rancher/k3s/k3s.yaml kubectl apply -f ../infra/argo/cd/install.yaml
 
 # Add top level argo cd application resource - this will auto discover all other applications
-kubectl apply -f ../infra/application.yaml
+KUBECONFIG=/etc/rancher/k3s/k3s.yaml kubectl apply -f ../infra/application.yaml
 
+
+# KUBECONFIG=/etc/rancher/k3s/k3s.yaml kubectl delete -f ../infra/application.yaml
+# KUBECONFIG=/etc/rancher/k3s/k3s.yaml kubectl delete -f ../infra/argo/cd/install.yaml
